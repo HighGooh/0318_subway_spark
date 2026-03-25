c440716d400ef49d0895f5ebc0a1b6671afa2f6a
leeej9801-max <leeej9801@gmail.com>
2026-03-25 14:53:43 +0900

    가영 기능 추가

--- backend/src/core/queries.py
+++ backend/src/core/queries.py
@@ -121,8 +121,8 @@ def get_station_history_sql():
         ORDER BY `연도` ASC
     """
 
-# 어린이날 데이터 수집 SQL
-KIDSDAY_SEARCH_SQL = """
+# 어린이날 연별 데이터 수집 SQL
+KIDSDAY_YEAR_SQL = """
             SELECT `역명`, `날짜`, `구분`, `합계`, `순위`
             FROM (
                 SELECT `역명`, `날짜`, `구분`,
@@ -135,3 +135,16 @@ KIDSDAY_SEARCH_SQL = """
             WHERE `순위` <= 5
             ORDER BY `날짜` ASC, `합계` DESC
         """
+
+# 어린이날 역별 데이터 수집 SQL
+KIDSDAY_STATION_SQL = """
+                    SELECT 
+                    `역명`,
+                    LEFT(`날짜`, 4) AS `년도`, 
+                    SUM(CAST(`10~11` AS INT) + CAST(`11~12` AS INT) + CAST(`12~13` AS INT)) AS `합계`
+                FROM kidsDayStationTable
+                WHERE `구분` = '하차'
+                and `날짜` like '%05-05'
+                GROUP BY `역명`,`년도`
+                order by `년도`;
+                    """
